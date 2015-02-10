e8b6eb790d37768509d79bd28636fdf9d00eb019
Sanda Golcea <sanda.golcea@gmail.com>
2015-02-10 15:12:18 +0000

    Added basic animation with crSpline

--- motion.js
+++ motion.js
@@ -2,5 +2,27 @@
 $(document).ready(function(){
   // checking that jquery works!!
   console.log("Ready to go!!");
-  $('#mario').animate({top: "+=50px"}, 'slow');
+  // moving Mario 50px
+  // $('#mario').animate({top: "+=50px"}, 'slow');
+
+
+  var position = document.getElementById(1).getBoundingClientRect();
+  
+  var top1 = position.top; 
+  var bottom1 = position.bottom; 
+
+
+  var position = document.getElementById(2).getBoundingClientRect();
+  var top2 = position.top; 
+  var bottom2 = position.bottom; 
+
+
+  $('#mario').click(function(){
+      $(this).animate(
+            { crSpline: $.crSpline.buildSequence([ [top1, bottom1], [0, 50], [top1, bottom1], [80, 50], [top2, bottom2] ]) },
+            5000
+      );
+  });
+
 })
+
