4a9a3d2bf65f0cdf2ad688f36d91db12e1588a11
Sanda Golcea <sanda.golcea@gmail.com>
2015-02-10 17:31:05 +0000

    changes image, dynamically positions it

--- motion.js
+++ motion.js
@@ -1,25 +1,25 @@
-// this js is for adding a motion move between squares
+// javascript file for adding a motion move between squares
 $(document).ready(function(){
-  // checking that jquery works!!
-  console.log("Ready to go!!");
-  // moving Mario 50px
-  // $('#mario').animate({top: "+=50px"}, 'slow');
-
-
-  var position = document.getElementById(1).getBoundingClientRect();
   
-  var top1 = position.top; 
-  var bottom1 = position.bottom; 
+  var position = document.getElementById(1).getBoundingClientRect();
+  var mario = document.getElementById('mario').getBoundingClientRect();
+  
+  var x = ( position.right - (position.width/2) ) - (mario.width/2); 
+  var y = (position.bottom - position.height/2) - (mario.height/2); 
+  $('#mario').hide();
+  $('#mario').css("top", y);
+  $('#mario').css("left", x);
+  $('#mario').fadeIn("slow");
 
-
-  var position = document.getElementById(2).getBoundingClientRect();
-  var top2 = position.top; 
-  var bottom2 = position.bottom; 
+  // Positioning testing purposes:
+  // console.log("right: "+ position.right+" width: " + position.width+" mario width: "+ mario.width);
+  // console.log("x : " + x);
+  // console.log("y : " + y);
 
 
   $('#mario').click(function(){
       $(this).animate(
-            { crSpline: $.crSpline.buildSequence([ [top1, bottom1], [0, 50], [top1, bottom1], [80, 50], [top2, bottom2] ]) },
+            { crSpline: $.crSpline.buildSequence([ [x,y], [0,100], [100,100], [100,0], [0,0], [x,y] ]) },
             5000
       );
   });
