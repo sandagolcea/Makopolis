2c4d3495a09338640271e9609b309f84e5ebc7f1
stepholdcorn <stephieoldcorn@gmail.com>
2015-02-10 18:27:19 +0000

    Adds a second square and motion between them

--- motion.js
+++ motion.js
@@ -1,27 +1,31 @@
 // javascript file for adding a motion move between squares
 $(document).ready(function(){
-  
-  var position = document.getElementById(1).getBoundingClientRect();
+
   var mario = document.getElementById('mario').getBoundingClientRect();
-  
-  var x = ( position.right - (position.width/2) ) - (mario.width/2); 
-  var y = (position.bottom - position.height/2) - (mario.height/2); 
+
+  var position = document.getElementById('sq1').getBoundingClientRect();
+  var x = (position.right - (position.width/2) ) - (mario.width/2);
+  var y = (position.bottom - position.height/2) - (mario.height/2);
+
   $('#mario').hide();
   $('#mario').css("top", y);
   $('#mario').css("left", x);
   $('#mario').fadeIn("slow");
+
+  var position2 = document.getElementById('sq2').getBoundingClientRect();
+  var x2 = (position2.right - (position2.width/2) ) - (mario.width/2);
+  var y2 = (position2.bottom - position2.height/2) - (mario.height/2);
 
   // Positioning testing purposes:
   // console.log("right: "+ position.right+" width: " + position.width+" mario width: "+ mario.width);
   // console.log("x : " + x);
   // console.log("y : " + y);
 
-
   $('#mario').click(function(){
-      $(this).animate(
-            { crSpline: $.crSpline.buildSequence([ [x,y], [0,100], [100,100], [100,0], [0,0], [x,y] ]) },
-            5000
-      );
+    $(this).animate(
+          { crSpline: $.crSpline.buildSequence([ [x,y], [x2,y2], ]) },
+          5000
+    );
   });
 
 })
