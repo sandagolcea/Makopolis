5d7436dd432c80547fc3aeae9f7b10f2f32c178b
Luke Clewlow <luke.clewlow@gmail.com>
2015-02-11 10:03:45 +0000

    integrated jquery so sonic only moves if you get the correct answer

--- motion.js
+++ motion.js
@@ -1,6 +1,8 @@
 // javascript file for adding a motion move between squares
 $(document).ready(function(){
 
+  var question = new Question();
+
   var mario = document.getElementById('mario').getBoundingClientRect();
 
   var position = document.getElementById('sq1').getBoundingClientRect();
@@ -21,11 +23,14 @@ $(document).ready(function(){
   // console.log("x : " + x);
   // console.log("y : " + y);
 
-  $('#mario').click(function(){
-    $(this).animate(
+  $('.qs').on('click', function() {
+    var value = parseInt($(this).attr("value"));
+    if(question.answer(value, question.question1CorrectAnswer)===true){
+      $('#mario').animate(
           { crSpline: $.crSpline.buildSequence([ [x,y], [x2,y2], ]) },
           5000
-    );
+      );
+    };
   });
 
 })
