97169326d9f7d9406982fce15b6bfaf7cc4df013
Sanda Golcea <sanda.golcea@gmail.com>
2015-02-12 10:39:10 +0000

    Changed server port to match process.env.PORT - for heroku

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ var app = express();
 
 var server = http.createServer(app);
 
+// getting the env port for heroku
+app.set('port', (process.env.PORT || 5000))
+
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
@@ -16,6 +19,10 @@ app.get('/', function(request, response){
 });
 
 
-server.listen(3000, function(){
-  console.log("Up and running on localhost:3000")
-});
+// server.listen(3000, function(){
+//   console.log("Up and running on localhost:3000")
+// });
+// modified prev 3 lines with next 3 for heroku purposes
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
