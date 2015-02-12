04f96dea1085ac57e5f9c22caba2d0cbfc96d653
Sanda Golcea <sanda.golcea@gmail.com>
2015-02-12 14:15:21 +0000

    Basic sockets added locally

--- server.js
+++ server.js
@@ -1,28 +1,21 @@
 var http = require('http');
-
-
 var express = require('express');
 var app = express();
-
 var server = http.createServer(app);
-
-// getting the env port for heroku
-app.set('port', (process.env.PORT || 5000))
+var io = require('socket.io')(server);
+var port = process.env.PORT || 5000 ;
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
-
-
 
 app.get('/', function(request, response){
   response.render('index');
 });
 
+io.on('connection', function(client) {
+  console.log('Client connected');
+})
 
-// server.listen(3000, function(){
-//   console.log("Up and running on localhost:3000")
-// });
-// modified prev 3 lines with next 3 for heroku purposes
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
+server.listen(port, function(){
+  console.log("Node app is running at localhost:" + port);
 })
