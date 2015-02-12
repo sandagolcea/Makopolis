c92d7a84537633e4bd84f5d024f31c94f0a7500e
Luke Clewlow <luke.clewlow@gmail.com>
2015-02-12 11:59:43 +0000

    updated tests, and fucntionality to check answers are correct

--- spec/bankSpec.js
+++ spec/bankSpec.js
@@ -7,18 +7,28 @@ describe ("Bank", function() {
   });
 
 
-  describe("storing questions", function(){ß
+  describe("storing questions", function(){
 
-  	it("should be able read the question", function(){
-  		expect(bank.questions('1')).toEqual('What is 2 + 2?');
-  	});
+    it("should be able read the question", function(){
+      expect(bank.questions('1')).toEqual('What is 2 + 2?');
+    });
 
     it('should be able to provide list of answers', function() {
-      expect(bank.choices('1')).toEqual([3, 5, 4, 2863]);
+      spyOn(bank, 'choice1').and.returnValue([3, 5, 4, 2863]);
+      expect(bank.choice1('1')).toEqual([3, 5, 4, 2863]);
     });
 
     it('should be able to provide THE answer', function() {
-      expect(bank.answer('1')).toEqual(4);
+      spyOn(bank, 'correctAnswer').and.returnValue(4);
+      expect(bank.correctAnswer('1')).toEqual(4);
+    });
+
+    it('should know when the player gets the correct answer', function() {
+      expect(bank.verifyAnswer('1', 4)).toBe(true);
+    });
+
+    it('should know when the player gets the wrong answer', function(){
+      expect(bank.verifyAnswer('1', 5)).toBe(false);
     });
 
   });
