5a96ee675726c0a6623645f5b1ec009323174756
stepholdcorn <stephieoldcorn@gmail.com>
2015-02-13 09:25:50 +0000

    Additional changes to testing to account for question answers being strings

--- spec/bankSpec.js
+++ spec/bankSpec.js
@@ -14,13 +14,13 @@ describe ("Bank", function() {
     });
 
     it('should be able to provide list of answers', function() {
-      spyOn(bank, 'choice1').and.returnValue([3, 5, 4, 2863]);
-      expect(bank.choice1('1')).toEqual([3, 5, 4, 2863]);
+      spyOn(bank, 'choice1').and.returnValue(['array.inject', 'array.select{|number| number + number}/array', '(array.inject(:+)/array.length.to_f).round', '(array.join)/array']);
+      expect(bank.choice1('1')).toEqual(['array.inject', 'array.select{|number| number + number}/array', '(array.inject(:+)/array.length.to_f).round', '(array.join)/array']);
     });
 
     it('should be able to provide THE answer', function() {
-      spyOn(bank, 'correctAnswer').and.returnValue(4);
-      expect(bank.correctAnswer('1')).toEqual(4);
+      spyOn(bank, 'correctAnswer').and.returnValue('array.inject(:+)/array.length.to_f).round');
+      expect(bank.correctAnswer('1')).toEqual('array.inject(:+)/array.length.to_f).round');
     });
 
     it('should know when the player gets the correct answer', function() {
@@ -28,7 +28,7 @@ describe ("Bank", function() {
     });
 
     it('should know when the player gets the wrong answer', function(){
-      expect(bank.verifyAnswer(0, 5)).toBe(false);
+      expect(bank.verifyAnswer(1, 'array.reverse')).toBe(false);
     });
 
     it('cannot display same question twice', function() {
