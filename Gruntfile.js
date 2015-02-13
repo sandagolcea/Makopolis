ca75a4b978fc4125fc27dea3dc17e46c32fafa0f
Luke Clewlow <luke.clewlow@gmail.com>
2015-02-13 16:05:25 +0000

    update coverage test

--- Gruntfile.js
+++ Gruntfile.js
@@ -70,7 +70,6 @@ module.exports = function(grunt) {
  });
 
 
-
  grunt.loadNpmTasks('grunt-contrib-jshint');
  grunt.loadNpmTasks('grunt-contrib-jasmine');
  grunt.loadNpmTasks('grunt-contrib-watch');
